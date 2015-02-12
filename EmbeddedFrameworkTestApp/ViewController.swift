8015f51baad4d7e4afbffd5415a711534301b919
Daniel R <danramteke@gmail.com>
2015-02-12 13:22:02 -0500

    added Alamofire via Carthage

--- EmbeddedFrameworkTestApp/ViewController.swift
+++ EmbeddedFrameworkTestApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        Alamofire.request(.GET, "http://google.com", parameters: nil).responseString { (_,_,data,_) in
+            println(data)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
